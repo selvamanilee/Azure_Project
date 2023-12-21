3da5e3f3c6ca2dbf054adaf62b9f748d5f8100fc
selvamani.r <selvamani.r@saint-gobain.com>
2023-12-21 11:57:01 +0530

    changes

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace Azure_Project
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int Count { get; set; }
     }
 }
